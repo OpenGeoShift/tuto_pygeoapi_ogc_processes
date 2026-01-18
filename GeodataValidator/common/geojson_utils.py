46cdef89991fe127f43fc4c5ac8f64e6838b712d
Matshopp <matthieu.schopp@gmail.com>
2026-01-18 17:43:18 +0100

    back to pydantic-geojson

--- GeodataValidator/common/geojson_utils.py
+++ GeodataValidator/common/geojson_utils.py
@@ -1,14 +1,14 @@
 import logging
-import geojson_validator
-#from pydantic_geojson import FeatureCollectionModel
-#from pydantic import ValidationError
+#import geojson_validator
+from pydantic_geojson import FeatureCollectionModel
+from pydantic import ValidationError
 
 LOGGER = logging.getLogger(__name__)
 #geojson_validator.configure_logging(enabled=False)
 
 class GeoJsonUtils:
 
-    """
+
     def geojson_isvalid(self, geojson: dict) -> bool:
         try:
             feature_collection = FeatureCollectionModel(**data)
@@ -19,6 +19,7 @@ class GeoJsonUtils:
 
     def validate_geojson_geometry(self, geojson: dict) -> bool:
         return True
+
     """
 
     def geojson_isvalid(self, geojson: dict) -> bool:
@@ -28,3 +29,4 @@ class GeoJsonUtils:
     def validate_geojson_geometry(self, geojson: dict) -> bool:
         validity_report = geojson_validator.validate_geometries(geojson)
         return validity_report
+    """
